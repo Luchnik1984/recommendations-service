e715e4c84155f7f225a9986ea9be17c29996c66c
Luchnik1984 <luchnik1984@mail.ru>
2025-11-02 09:00:02 +0300

    initial commit

---
+++ src/test/java/org/skypro/bank/star/recommendations_service/RecommendationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.bank.star.recommendations_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
