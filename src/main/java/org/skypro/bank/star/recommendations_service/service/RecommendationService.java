1a1eb756f4103c7207bd4eada985ccd246a50927
ArtemGRB <artemgrb@gmail.com>
2025-11-11 22:10:57 +0700

    US9 добавление методов в сервис для: только динамических и только статических правил

--- src/main/java/org/skypro/bank/star/recommendations_service/service/RecommendationService.java
+++ src/main/java/org/skypro/bank/star/recommendations_service/service/RecommendationService.java
@@ -6,10 +6,10 @@ import org.skypro.bank.star.recommendations_service.rule.RecommendationRuleSet;
 import org.skypro.bank.star.recommendations_service.rule.executor.QueryExecutorFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 public class RecommendationService {
@@ -22,25 +22,67 @@ public class RecommendationService {
         this.ruleSets = ruleSets;
         this.queryExecutorFactory = queryExecutorFactory;
     }
-//
-//    public RecommendationService(List<RecommendationRuleSet> ruleSets) {
-//        this.ruleSets = ruleSets;
-//    }
 
+    /**
+     * Метод получения рекомендаций для пользователя по его идентификатору
+     * используются динамические и статические правила
+     *
+     * @param userId идентификатор пользователя
+     * @return список рекомендаций DTO
+     */
     public RecommendationResponse getRecommendationsForUser(UUID userId) {
 
+        List<RecommendationDTO> recommendations = addToListRecommendationWithStaticRulesForUserID(
+                ruleSets,
+                userId,
+                queryExecutorFactory.createResponseWithRecommendations(userId));
 
+        return new RecommendationResponse(userId, recommendations);
+    }
+
+    /**
+     * Метод получения рекомендаций для пользователя по его идентификатору
+     * используются только динамические правила
+     *
+     * @param userId идентификатор пользователя
+     * @return список рекомендаций DTO
+     */
+    public RecommendationResponse getRecommendationsForUserWithoutStaticRule(UUID userId) {
         List<RecommendationDTO> recommendations = queryExecutorFactory.createResponseWithRecommendations(userId);
+        return new RecommendationResponse(userId, recommendations);
+    }
+
+    /**
+     * Метод получения рекомендаций для пользователя по его идентификатору
+     * используются только статические правила
+     *
+     * @param userId идентификатор пользователя
+     * @return список рекомендаций DTO
+     */
+    public RecommendationResponse getRecommendationsForUserWithoutDynamicRules(UUID userId) {
+        List<RecommendationDTO> recommendations =
+                addToListRecommendationWithStaticRulesForUserID(ruleSets, userId, new ArrayList<>());
+        return new RecommendationResponse(userId, recommendations);
+    }
+
+    /**
+     * Метод добавления в список DTO рекомендаций для пользователя по его идентификатору
+     * для статических правил
+     *
+     * @param ruleSets        список статических правил
+     * @param userId          идентификатор пользователя
+     * @param recommendations список DTO рекомендаций
+     * @return обновленный список DTO рекомендаций
+     */
+    private List<RecommendationDTO> addToListRecommendationWithStaticRulesForUserID(
+            List<RecommendationRuleSet> ruleSets,
+            UUID userId,
+            List<RecommendationDTO> recommendations) {
 
         for (RecommendationRuleSet ruleSet : ruleSets) {
             Optional<RecommendationDTO> recommendationDTO = ruleSet.checkUser(userId);
-            RecommendationDTO recommendationDTO1 = recommendationDTO.orElse(null);
-            if (recommendationDTO1 != null)
-                recommendations.add(recommendationDTO1);
-
+            recommendationDTO.ifPresent(recommendations::add);
         }
-
-        return new RecommendationResponse(userId, recommendations);
-
+        return recommendations;
     }
 }
