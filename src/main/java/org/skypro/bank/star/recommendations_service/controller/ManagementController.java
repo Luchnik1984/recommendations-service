fa72c42d3c8df938c7f9a0899753f6104a508d08
Luchnik1984 <luchnik1984@mail.ru>
2025-11-19 22:29:23 +0300

    US10-12: Add TTL in cache

--- src/main/java/org/skypro/bank/star/recommendations_service/controller/ManagementController.java
+++ src/main/java/org/skypro/bank/star/recommendations_service/controller/ManagementController.java
@@ -1,22 +1,51 @@
 package org.skypro.bank.star.recommendations_service.controller;
 
 import org.skypro.bank.star.recommendations_service.service.CacheManagementService;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+
 @RestController
-@RequestMapping("/management/clear-caches")
+@RequestMapping("/management")
 public class ManagementController {
 
-    CacheManagementService cacheManagementService;
+    private final CacheManagementService cacheManagementService;
+    private final BuildProperties buildProperties;
 
-    public ManagementController(CacheManagementService cacheManagementService) {
+    public ManagementController(CacheManagementService cacheManagementService,
+                                BuildProperties buildProperties)
+    {
         this.cacheManagementService = cacheManagementService;
+        this.buildProperties = buildProperties;
     }
 
-    @PostMapping("")
-    public void clearAllCaches(){
+    /**
+     * Эндпоинт для принудительной очистки всех кэшей сервиса.
+     * POST /management/clear-caches
+     * Соответствует требованию - сброс кэша рекомендаций
+     */
+    @PostMapping("/clear-caches")
+    public ResponseEntity<Void> clearAllCaches(){
         cacheManagementService.clearAllCaches();
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * Эндпоинт для получения информации о сборке сервиса.
+     * GET /management/info
+     * Соответствует требованию части 3: информация из pom.xml
+     */
+    @GetMapping("/info")
+    public ResponseEntity<Map<String, String>> getServiceInfo() {
+        return ResponseEntity.ok(Map.of(
+                "name", buildProperties.getName(),
+                "version", buildProperties.getVersion()
+        ));
     }
 }
