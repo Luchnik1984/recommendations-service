2ff625fd7dd72de10a481369e8e677c654513ce5
Admin <git@github.com:Luchnik1984/UsingVariablesApplication.git>
2025-11-20 09:26:56 +0300

    added logging of cache endpoints

--- src/main/java/org/skypro/bank/star/recommendations_service/service/CacheManagementService.java
+++ src/main/java/org/skypro/bank/star/recommendations_service/service/CacheManagementService.java
@@ -1,19 +1,24 @@
 package org.skypro.bank.star.recommendations_service.service;
 
 import org.skypro.bank.star.recommendations_service.repository.CachedUserDataRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CacheManagementService {
 
     CachedUserDataRepository cachedUserDataRepository;
+    private static final Logger logger = LoggerFactory.getLogger(CacheManagementService.class);
 
     public CacheManagementService(CachedUserDataRepository cachedUserDataRepository) {
         this.cachedUserDataRepository = cachedUserDataRepository;
     }
 
-    public void clearAllCaches() {
+    public void clearAllCaches()
+    { logger.info("Clearing all caches");
         cachedUserDataRepository.clearAllCaches();
+        logger.info("All caches cleared successfully");
     }
 
 }
